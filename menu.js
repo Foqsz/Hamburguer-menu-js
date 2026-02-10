f3fb662d898f3ef1a1e223af4634ec92aa9c19b8
Foqsz <contatovictorvinicius05@gmail.com>
2026-02-10 11:32:04 -0300

    add initial implementation of hamburger menu with responsive design

---
+++ menu.js
@@ -0,0 +1,51 @@
+const btnMenu = document.getElementById("hamburguer-button")
+const menu = document.getElementById("menu");
+
+btnMenu.classList.add("hamburguer-button-js-enabled");
+
+function closeMenu(){
+    btnMenu.setAttribute("aria-expanded", "false");
+    menu.setAttribute("aria-hidden", "true");
+    menu.classList.add("menu-closed");
+}
+
+closeMenu();
+
+btnMenu.addEventListener("click", function(){
+    let expanded = this.getAttribute("aria-expanded") === "true" ? true : false
+
+    document.removeEventListener("click", closeMenu)
+    if(expanded){
+        menu.classList.add("menu-closed");
+    }
+    else{
+        menu.classList.remove("menu-closed");
+    }
+
+    this.setAttribute("aria-expanded", !expanded);
+    menu.setAttribute("aria-hidden", expanded);
+
+    setTimeout(function(){
+        if(!expanded)
+            document.addEventListener("click", closeMenu)
+    }, 100)
+    
+})
+
+const mediaQuery = window.matchMedia('(min-width: 768px)');
+
+function handleMediaQueryChange(event){
+    if(event.matches){
+        menu.setAttribute("aria-hidden", "false");
+        btnMenu.setAttribute("aria-expanded", "true");
+        menu.classList.remove("menu-closed");
+    }
+    else{
+        menu.setAttribute("aria-hidden", "true");
+        btnMenu.setAttribute("aria-expanded", "false");
+        menu.classList.add("menu-closed");
+    }
+}
+
+mediaQuery.addEventListener("change", handleMediaQueryChange)
+handleMediaQueryChange(mediaQuery);
